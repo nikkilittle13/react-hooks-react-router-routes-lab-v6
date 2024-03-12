4553faa57c80cc0519f4c4720900747ab72d0821
Nikki Little <nikkilittle13@gmail.com>
2024-03-12 09:20:31 -0400

    completed lab

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import "./index.css";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import routes from "./routes";
+import Home from "./pages/Home";
+
+const router = createBrowserRouter(routes)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render();
+root.render(<RouterProvider router={router} />);
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,30 @@
-
+import Directors from "./pages/Directors";
+import Actors from "./pages/Actors";
+import Movie from "./pages/Movie";
+import Home from "./pages/Home"
+import ErrorPage from "./pages/ErrorPage"
 
 const routes = [
-   
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "/directors",
+    element: <Directors />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "/actors",
+    element: <Actors />,
+    errorElement: <ErrorPage />
+  },
+  {
+    path: "/movie/:id",
+    element: <Movie />,
+    errorElement: <ErrorPage />
+  }
   ];
 
 export default routes;
